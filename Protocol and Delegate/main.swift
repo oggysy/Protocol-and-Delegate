230e08ffc50397d64299d15d728ad4c50382f494
oggysy <ogisoys@gmail.com>
2023-02-05 14:14:08 +0900

    second commit

--- Protocol and Delegate/main.swift
+++ Protocol and Delegate/main.swift
@@ -1,8 +1,8 @@
 
+// BootCampのセクション13のNo.147「An Example of Protocols and Delegates in Practice
 protocol AdvancedLifeSupport {
     func perfomCPR()
 }
-
 
 class EmergencyCallHandler {
     var delegate: AdvancedLifeSupport?
@@ -11,7 +11,6 @@ class EmergencyCallHandler {
         delegate?.perfomCPR()
     }
 }
-
 
 class Pramedic: AdvancedLifeSupport {
     
@@ -24,10 +23,41 @@ class Pramedic: AdvancedLifeSupport {
     }
 }
 
-
 let emilio = EmergencyCallHandler()
 let pete = Pramedic(handeler: emilio)
 
-print(emilio.medicalEmergency())
+emilio.medicalEmergency()
 
 
+
+
+//「Swift実践入門」p289 13.2 デリゲートパターンでのサンプルコード
+
+protocol GameDelegate: AnyObject {
+    var numberOfPlayers: Int { get }
+    func gameDidStart(_ game: Game)
+    func gameDidEnd(_ game: Game)
+}
+
+class TwoPersonsGameDelegate : GameDelegate {
+    var numberOfPlayers: Int { return 2}
+    func gameDidStart(_ game: Game) { print("Game start")}
+    func gameDidEnd(_ game: Game) { print("Game end")}
+}
+
+class Game {
+    weak var delegate: GameDelegate?
+    
+    func start() {
+        print("Number of players is \(delegate?.numberOfPlayers ?? 1)")
+        delegate?.gameDidStart(self)
+        print("Playing")
+        delegate?.gameDidEnd(self)
+    }
+}
+
+
+let delegate = TwoPersonsGameDelegate()
+let twoPersonsGame = Game()
+twoPersonsGame.delegate = delegate
+twoPersonsGame.start()
